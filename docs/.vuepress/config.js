fe34940ca20e279bd1650f9cfe2eca3c79088c00
石松 <rgkjhshi@gmail.com>
2024-01-16 23:37:29 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,8 +24,22 @@ module.exports = {
             }
         ],
         lastUpdated: '上次更新', // string | boolean
-        // 自动生成侧边栏
-        sidebar: 'auto',
+        // 侧边栏
+        sidebar: [
+            {
+                title: '分组一',
+                //可选, 标题的跳转链接，应为绝对路径且必须存在
+                path: '/question/',
+                children: [
+                    '/question/0001.两数之和',
+                    '/question/0001.两数之和'
+                ]
+            },
+            {
+                title: '分组二',
+                children: [],
+            }
+        ],
         // 假定是 GitHub. 同时也可以是一个完整的 GitHab URL
         repo: 'rgkjhshi/leetcode',
     },
